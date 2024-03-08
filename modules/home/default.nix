69c51dfb9b09e1c70ab587c6a760cdd131101277
relogit <159662138+relogit@users.noreply.github.com>
2024-03-07 22:14:42 -0600

    Refreshed archive

---
+++ modules/home/default.nix
@@ -0,0 +1,8 @@
+{
+  imports = [
+    # when you make a module you'll put it here as the relative path
+    # for example a hyprland module could be './hyprland.nix'
+    # you can also import directories if you wanna sort your modules, which looks like './applications' as an example
+    # if you import a directory the directory also needs a default.nix like this
+  ];
+}
